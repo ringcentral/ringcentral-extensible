c298b1f9ab4278a3d5ddd5dcc1877d0c5de213b2
Tyler Long <tyler4long@gmail.com>
2020-04-03 20:50:49 -0700

    Try to generate definitions

--- index.ts
+++ index.ts
@@ -1,5 +1,32 @@
 import axios from 'axios'
 
+class RestClient {
+    static sandboxServer = "https://platform.devtest.ringcentral.com"
+    static productionServer = "https://platform.ringcentral.com"
+
+    clientId: string
+    clientSecret: string
+    server: string
+    appName = "Unknown"
+    appVersion = "0.0.1"
+
+    constructor(clientId: string, clientSecret: string,  server: string,  appName = "Unknown", appVersion = "0.0.1")
+    {
+        this.clientId = clientId
+        this.clientSecret = clientSecret
+        this.server = server
+        this.appName = appName
+        this.appVersion = appVersion
+    }
+
+    authorize(getTokenRequest: number): number
+    authorize(username: string, extension: string, password: string): number
+    authorize(arg1: any, extension?: string, password?: string): number
+    {
+        return 1
+    }
+} 
+
 ;(async () => {
     const r = await axios({
         method: 'get',
