40c8aabae6704387aff43ac56feeef1d0a859efd
Tyler Long <tyler4long@gmail.com>
2020-04-05 19:14:40 -0700

    jest.setTimeout(16000)

--- test/http_get.spec.js
+++ test/http_get.spec.js
@@ -1,11 +1,14 @@
 /* eslint-env jest */
 import RestClient from '../src/index'
 
+jest.setTimeout(16000)
+
 describe('HTTP GET', () => {
   test('get extension info', async () => {
     const rc = new RestClient(process.env.RINGCENTRAL_CLIENT_ID, process.env.RINGCENTRAL_CLIENT_SECRET, process.env.RINGCENTRAL_SERVER_URL)
     await rc.authorize(process.env.RINGCENTRAL_USERNAME, process.env.RINGCENTRAL_EXTENSION, process.env.RINGCENTRAL_PASSWORD)
-    const r = await rc.get('/restapi/v1.0/account/~/extension/~')
-    console.log(r)
+    const extensionInfo = await rc.get('/restapi/v1.0/account/~/extension/~')
+    expect(extensionInfo).not.toBeNull()
+    expect(extensionInfo.id).not.toBeNull()
   })
 })
