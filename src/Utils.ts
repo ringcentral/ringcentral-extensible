03c15660583d8057fac420a51715831a4097f60b
Tyler Long <tyler4long@gmail.com>
2020-04-15 13:21:57 -0700

    First version of getFormData

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,8 +1,38 @@
 import FormData from 'form-data'
 
+import Attachment from './definitions/Attachment'
+
 class Utils {
-  static getFormData(obj: any): FormData {
-    return null
+  static getFormData(...objs): FormData {
+    const formData = new FormData({})
+    const obj = Object.assign({}, ...objs)
+    const jsonRequest = {}
+    const attachments: Attachment[] = []
+    for(const key in Object.keys(obj)) {
+      let value = obj[key]
+      if(value === undefined || value === null) {
+        continue
+      }
+      if(value instanceof Attachment) {
+        attachments.push(value)
+      }
+      if (Array.isArray(value) && value[0] instanceof Attachment) {
+        attachments.push(...value)
+      } else {
+        jsonRequest[key] = value
+      }
+    }
+    formData.append('files[]', JSON.stringify(jsonRequest), {
+      filename: 'request.json',
+      contentType: 'application/json'
+    })
+    for(const attachment of attachments) {
+      formData.append('files[]', attachment.content, {
+        filename: attachment.filename,
+        contentType: attachment.contentType,
+      })
+    }
+    return formData
   }
 }
 
