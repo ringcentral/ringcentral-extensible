febef786511a4556139cdbacecb1640804728273
Tyler Long <tyler4long@gmail.com>
2020-04-23 20:40:46 -0700

    Add low level sms test

--- test/low_level_api.spec.ts
+++ test/low_level_api.spec.ts
@@ -8,6 +8,21 @@ import Utils from '../src/Utils'
 jest.setTimeout(16000)
 
 describe('low level API', () => {
+  test('sms', async () => {
+    const rc = new RestClient(process.env.RINGCENTRAL_CLIENT_ID!, process.env.RINGCENTRAL_CLIENT_SECRET!, process.env.RINGCENTRAL_SERVER_URL!)
+    await rc.authorize(process.env.RINGCENTRAL_USERNAME!, process.env.RINGCENTRAL_EXTENSION!, process.env.RINGCENTRAL_PASSWORD!)
+    const messageInfo = await rc.post('/restapi/v1.0/account/~/extension/~/sms', {
+      from: {
+        phoneNumber: process.env.RINGCENTRAL_USERNAME!
+      },
+      to: [{
+        phoneNumber: process.env.RINGCENTRAL_RECEIVER
+      }],
+      text: 'hello world'
+    })
+    expect(messageInfo).not.toBeUndefined()
+    expect(messageInfo.id).not.toBeUndefined()
+  })
   test('fax', async () => {
     const rc = new RestClient(process.env.RINGCENTRAL_CLIENT_ID!, process.env.RINGCENTRAL_CLIENT_SECRET!, process.env.RINGCENTRAL_SERVER_URL!)
     await rc.authorize(process.env.RINGCENTRAL_USERNAME!, process.env.RINGCENTRAL_EXTENSION!, process.env.RINGCENTRAL_PASSWORD!)
