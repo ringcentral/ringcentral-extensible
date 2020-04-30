229a204bbfa4e6314a2e6b210102cb11f6c831de
Tyler Long <tyler4long@gmail.com>
2020-04-29 18:28:34 -0700

    Fix format issues

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,7 @@ class RestClient {
   token?: TokenInfo
   handleRateLimit?: (boolean | number)
 
-  constructor(opts: ConstructorOpts) {
+  constructor (opts: ConstructorOpts) {
     this.clientId = opts.clientId
     this.clientSecret = opts.clientSecret
     this.server = opts.server
@@ -59,7 +59,7 @@ class RestClient {
     this.token = opts.token
   }
 
-  async request(httpMethod: Method, endpoint: string, content?: {}, queryParams?: {}, config?: {}): Promise<AxiosResponse<any>> {
+  async request (httpMethod: Method, endpoint: string, content?: {}, queryParams?: {}, config?: {}): Promise<AxiosResponse<any>> {
     const _config: AxiosRequestConfig = {
       method: httpMethod,
       url: endpoint,
@@ -81,14 +81,13 @@ class RestClient {
     }
     const r = await this.httpClient.request(_config)
 
-    if (r.status == 429 && this.handleRateLimit == false) {
+    if (r.status === 429 && this.handleRateLimit === false) {
       throw new RestException(r)
-    }
-    else if (r.status == 429 && (this.handleRateLimit == true || typeof this.handleRateLimit == 'number')) {
+    } else if (r.status === 429 && (this.handleRateLimit === true || typeof this.handleRateLimit === 'number')) {
 
       let delayTime = r.headers['x-rate-limit-window'] ? r.headers['x-rate-limit-window'] : 60
 
-      if (typeof this.handleRateLimit == 'number') {
+      if (typeof this.handleRateLimit === 'number') {
         delayTime = this.handleRateLimit
       }
 
@@ -99,33 +98,32 @@ class RestClient {
 
       return this.request(httpMethod, endpoint, content, queryParams, config)
 
-    }
-    else if (r.status < 200 || r.status > 299 && r.status != 429) {
+    } else if (r.status < 200 || r.status > 299 && r.status !== 429) {
       throw new RestException(r)
     }
     return r
   }
-  async get(endpoint: string, queryParams?: {}, config?: {}): Promise<AxiosResponse<any>> {
+  async get (endpoint: string, queryParams?: {}, config?: {}): Promise<AxiosResponse<any>> {
     return this.request('GET', endpoint, undefined, queryParams, config)
   }
-  async delete(endpoint: string, queryParams?: {}, config?: {}): Promise<AxiosResponse<any>> {
+  async delete (endpoint: string, queryParams?: {}, config?: {}): Promise<AxiosResponse<any>> {
     return this.request('DELETE', endpoint, undefined, queryParams, config)
   }
-  async post(endpoint: string, content?: {}, queryParams?: {}, config?: {}): Promise<AxiosResponse<any>> {
+  async post (endpoint: string, content?: {}, queryParams?: {}, config?: {}): Promise<AxiosResponse<any>> {
     return this.request('POST', endpoint, content, queryParams, config)
   }
-  async put(endpoint: string, content: {}, queryParams?: {}, config?: {}): Promise<AxiosResponse<any>> {
+  async put (endpoint: string, content: {}, queryParams?: {}, config?: {}): Promise<AxiosResponse<any>> {
     return this.request('PUT', endpoint, content, queryParams, config)
   }
-  async patch(endpoint: string, content: {}, queryParams?: {}, config?: {}): Promise<AxiosResponse<any>> {
+  async patch (endpoint: string, content: {}, queryParams?: {}, config?: {}): Promise<AxiosResponse<any>> {
     return this.request('PATCH', endpoint, content, queryParams, config)
   }
 
-  async authorize(getTokenRequest: GetTokenRequest): Promise<TokenInfo>
+  async authorize (getTokenRequest: GetTokenRequest): Promise<TokenInfo>
 
   // authorize(authCode, redirectUri)
-  async authorize(arg1: string, arg2: string, arg3?: string): Promise<TokenInfo>
-  async authorize(arg1: string | GetTokenRequest, arg2?: string, arg3?: string): Promise<TokenInfo> {
+  async authorize (arg1: string, arg2: string, arg3?: string): Promise<TokenInfo>
+  async authorize (arg1: string | GetTokenRequest, arg2?: string, arg3?: string): Promise<TokenInfo> {
     let getTokenRequest = new GetTokenRequest()
     if (arg1 instanceof GetTokenRequest) {
       getTokenRequest = arg1
@@ -151,7 +149,7 @@ class RestClient {
    *
    * @param opts PasswordLoginFlowOpts
    */
-  async login(opts: PasswordLoginFlowOpts) {
+  async login (opts: PasswordLoginFlowOpts) {
     const getTokenRequest = new GetTokenRequest()
 
     getTokenRequest.grant_type = 'password'
@@ -171,7 +169,7 @@ class RestClient {
    *
    * @param refreshToken Refresh Token
    */
-  async refresh(refreshToken?: string): Promise<TokenInfo> {
+  async refresh (refreshToken?: string): Promise<TokenInfo> {
     const tokenToRefresh = refreshToken ?? this.token?.refresh_token
     if (!tokenToRefresh) {
       throw new Error('tokenToRefresh must be specified.')
@@ -190,7 +188,7 @@ class RestClient {
    *
    * @param tokenToRevoke AccessToken
    */
-  async revoke(tokenToRevoke?: string) {
+  async revoke (tokenToRevoke?: string) {
     if (!tokenToRevoke && !this.token) { // nothing to revoke
       return
     }
@@ -205,7 +203,7 @@ class RestClient {
    *
    * @param apiVersion API version, currently the only valid value is 'v1.0'
    */
-  restapi(apiVersion: (string | null) = 'v1.0'): Restapi {
+  restapi (apiVersion: (string | null) = 'v1.0'): Restapi {
     return new Restapi(this, apiVersion)
   }
 
@@ -215,7 +213,7 @@ class RestClient {
    *
    * @param version SCIM API version, currently the only valid value is 'v2'
    */
-  scim(version: (string | null) = 'v2'): Scim {
+  scim (version: (string | null) = 'v2'): Scim {
     return new Scim(this, version)
   }
 }
