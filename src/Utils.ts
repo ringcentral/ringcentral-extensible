73d932f0df116613d2a9a9a4170b2bf71e9750fb
Tyler Long <tyler4long@gmail.com>
2020-04-30 13:49:45 -0700

    Add RestTraffic class

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,6 +1,54 @@
 import FormData from 'form-data'
 
 import { Attachment } from './definitions'
+import { AxiosResponse } from 'axios'
+
+export class RestResponse {
+  data?: any
+  status?: number
+  statusText?: string
+  headers?: any
+}
+
+export class RestRequest {
+  method?: string
+  baseURL?: string
+  url?: string
+  data?: string
+  headers?: any
+}
+
+export class RestTraffic {
+  axiosResponse: AxiosResponse
+  response: RestResponse
+  request: RestRequest
+  constructor (r: AxiosResponse) {
+    this.axiosResponse = r
+    this.response = {
+      data: r.data,
+      status: r.status,
+      statusText: r.statusText,
+      headers: r.headers
+    }
+    this.request = {
+      method: r.config.method,
+      baseURL: r.config.baseURL,
+      url: r.config.url,
+      data: r.config.data,
+      headers: r.config.headers
+    }
+  }
+  toString (): string {
+    return `HTTP ${this.axiosResponse.status} ${this.axiosResponse.statusText}${this.axiosResponse.data.message ? ` - ${this.axiosResponse.data.message}` : ''}
+
+    Response:
+    ${JSON.stringify(this.response, null, 2)}
+
+    Request:
+    ${JSON.stringify(this.request, null, 2)}
+    `
+  }
+}
 
 class Utils {
   static getFormData (...objs: any[]): FormData {
