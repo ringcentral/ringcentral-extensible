0a8ecb4788390caaa2e84c99aef6e25b5c9dc2eb
Tyler Long <tyler4long@gmail.com>
2020-05-02 19:31:15 -0700

    No more any!

--- src/Utils.ts
+++ src/Utils.ts
@@ -36,30 +36,31 @@ class Utils {
     `;
   }
 
-  static isAttachment(obj: any): boolean {
+  static isAttachment(obj: {}): boolean {
     return 'filename' in obj && 'content' in obj;
   }
 
-  static getFormData(...objs: any[]): FormData {
+  static getFormData(...objs: {}[]): FormData {
     const formData = new FormData({});
     const obj = Object.assign({}, ...objs);
-    const jsonRequest: any = {};
     const attachments: Attachment[] = [];
     for (const key of Object.keys(obj)) {
       const value = obj[key];
       if (value === undefined || value === null) {
+        delete obj[key];
         continue;
       }
       if (Utils.isAttachment(value)) {
         attachments.push(value);
+        delete obj[key];
+        continue;
       }
       if (Array.isArray(value) && Utils.isAttachment(value[0])) {
         attachments.push(...value);
-      } else {
-        jsonRequest[key] = value;
+        delete obj[key];
       }
     }
-    formData.append('files[]', JSON.stringify(jsonRequest), {
+    formData.append('files[]', JSON.stringify(obj), {
       filename: 'request.json',
       contentType: 'application/json',
     });
