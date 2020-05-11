5ca4938a96ebfa3e8a80930818e64ea2cafd7a37
Tyler Long <tyler4long@gmail.com>
2020-05-10 21:09:41 -0700

    Test WSG extension info

--- test/extension_info.spec.ts
+++ test/extension_info.spec.ts
@@ -1,23 +1,18 @@
 /* eslint-env jest */
-import RingCentral from '../src/index';
+import {createRingCentral} from './utils';
 
 jest.setTimeout(64000);
 
 describe('HTTP GET', () => {
   test('get extension info', async () => {
-    const rc = new RingCentral({
-      clientId: process.env.RINGCENTRAL_CLIENT_ID!,
-      clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
-      server: process.env.RINGCENTRAL_SERVER_URL!,
-    });
-    await rc.login({
-      username: process.env.RINGCENTRAL_USERNAME!,
-      extension: process.env.RINGCENTRAL_EXTENSION!,
-      password: process.env.RINGCENTRAL_PASSWORD!,
-    });
-    const extensionInfo = await rc.restapi().account().extension().get();
-    expect(extensionInfo).not.toBeUndefined();
-    expect(extensionInfo.id).not.toBeUndefined();
-    await rc.revoke();
+    const f = async (transport: 'https' | 'wss') => {
+      const rc = await createRingCentral(transport);
+      const extensionInfo = await rc.restapi().account().extension().get();
+      expect(extensionInfo).not.toBeUndefined();
+      expect(extensionInfo.id).not.toBeUndefined();
+      await rc.revoke();
+    };
+    await f('https');
+    await f('wss');
   });
 });
