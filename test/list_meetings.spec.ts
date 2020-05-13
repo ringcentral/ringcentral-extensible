856722626af11df1c7cd3112ee2caf6a45eaca13
Tyler Long <tyler4long@gmail.com>
2020-05-12 19:08:40 -0700

    More WSG test cases

--- test/list_meetings.spec.ts
+++ test/list_meetings.spec.ts
@@ -1,28 +1,24 @@
 /* eslint-env jest */
 import RingCentral from '../src/index';
 
+import {testRingCentral} from './utils';
+
 jest.setTimeout(64000);
 
 describe('list meetings', () => {
   test('default', async () => {
-    const rc = new RingCentral({
-      clientId: process.env.RINGCENTRAL_CLIENT_ID!,
-      clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
-      server: process.env.RINGCENTRAL_SERVER_URL!,
-    });
-    await rc.login({
-      username: process.env.RINGCENTRAL_USERNAME!,
-      extension: process.env.RINGCENTRAL_EXTENSION!,
-      password: process.env.RINGCENTRAL_PASSWORD!,
-    });
-    const meetingList = await rc
-      .restapi()
-      .account()
-      .extension()
-      .meeting()
-      .list();
-    expect(meetingList).not.toBeUndefined();
-    expect(meetingList.records).not.toBeUndefined();
-    await rc.revoke();
+    const testCase = async (rc: RingCentral) => {
+      const meetingList = await rc
+        .restapi()
+        .account()
+        .extension()
+        .meeting()
+        .list();
+      expect(meetingList).not.toBeUndefined();
+      expect(meetingList.records).not.toBeUndefined();
+      await rc.revoke();
+    };
+    testRingCentral(testCase);
+    testRingCentral(testCase, 'wss');
   });
 });
--- test/low_level_api.spec.ts
+++ test/low_level_api.spec.ts
@@ -5,39 +5,34 @@ import path from 'path';
 import RingCentral from '../src/index';
 import Utils from '../src/Utils';
 import {FaxResponse, GetMessageInfoResponse} from '../src/definitions';
+import {testRingCentral} from './utils';
 
 jest.setTimeout(64000);
 
 describe('low level API', () => {
   test('sms', async () => {
-    const rc = new RingCentral({
-      clientId: process.env.RINGCENTRAL_CLIENT_ID!,
-      clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
-      server: process.env.RINGCENTRAL_SERVER_URL!,
-    });
-    await rc.login({
-      username: process.env.RINGCENTRAL_USERNAME!,
-      extension: process.env.RINGCENTRAL_EXTENSION!,
-      password: process.env.RINGCENTRAL_PASSWORD!,
-    });
-    const r = await rc.post<GetMessageInfoResponse>(
-      '/restapi/v1.0/account/~/extension/~/sms',
-      {
-        from: {
-          phoneNumber: process.env.RINGCENTRAL_USERNAME!,
-        },
-        to: [
-          {
-            phoneNumber: process.env.RINGCENTRAL_RECEIVER,
+    const testCase = async (rc: RingCentral) => {
+      const r = await rc.post<GetMessageInfoResponse>(
+        '/restapi/v1.0/account/~/extension/~/sms',
+        {
+          from: {
+            phoneNumber: process.env.RINGCENTRAL_USERNAME!,
           },
-        ],
-        text: 'hello world',
-      }
-    );
-    const messageInfo = r.data;
-    expect(messageInfo).not.toBeUndefined();
-    expect(messageInfo.id).not.toBeUndefined();
-    await rc.revoke();
+          to: [
+            {
+              phoneNumber: process.env.RINGCENTRAL_RECEIVER,
+            },
+          ],
+          text: 'hello world',
+        }
+      );
+      const messageInfo = r.data;
+      expect(messageInfo).not.toBeUndefined();
+      expect(messageInfo.id).not.toBeUndefined();
+      await rc.revoke();
+    };
+    testRingCentral(testCase);
+    testRingCentral(testCase, 'wss');
   });
   test('fax', async () => {
     const rc = new RingCentral({
@@ -76,5 +71,6 @@ describe('low level API', () => {
     expect(messageInfo).not.toBeUndefined();
     expect(messageInfo.id).not.toBeUndefined();
     await rc.revoke();
+    // we don't test WSG because WSG doesn't support multipart/form-data
   });
 });
