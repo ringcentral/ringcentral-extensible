a15cffe003f65e1b856f748d71e85ce907fd339a
Tyler Long <tyler4long@gmail.com>
2020-06-04 10:04:48 -0700

    Fix issues in rate limit extension

--- src/extensions/rateLimit/index.ts
+++ src/extensions/rateLimit/index.ts
@@ -19,31 +19,28 @@ class RateLimitExtension extends SdkExtension {
       if (!this.enabled) {
         return request<T>(httpMethod, endpoint, content, queryParams, config);
       }
-      try {
-        const r = await request<T>(
-          httpMethod,
-          endpoint,
-          content,
-          queryParams,
-          config
-        );
-        return r;
-      } catch (e) {
-        if (e instanceof RestException) {
-          if (e.response.status === 429) {
-            const delaySeconds =
-              e.response.headers['x-rate-limit-window'] ?? 60;
-            await waitFor({interval: delaySeconds * 1000});
-            return request<T>(
-              httpMethod,
-              endpoint,
-              content,
-              queryParams,
-              config
-            );
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        try {
+          const r = await request<T>(
+            httpMethod,
+            endpoint,
+            content,
+            queryParams,
+            config
+          );
+          return r;
+        } catch (e) {
+          if (e instanceof RestException) {
+            if (e.response.status === 429) {
+              const delaySeconds =
+                e.response.headers['x-rate-limit-window'] ?? 60;
+              await waitFor({interval: delaySeconds * 1000});
+              continue;
+            }
           }
+          throw e;
         }
-        throw e;
       }
     };
   }
