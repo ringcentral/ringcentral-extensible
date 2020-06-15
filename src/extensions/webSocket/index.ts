86e1ac03179bf552e9873ffb2ba741e506a77f07
Tyler Long <tyler4long@gmail.com>
2020-06-15 16:54:10 -0700

    Comment out code for websocket extension

--- src/extensions/webSocket/index.ts
+++ src/extensions/webSocket/index.ts
@@ -1,18 +1,18 @@
-import RingCentral from '../..';
-import {AxiosResponse, Method} from 'axios';
-import {RestRequestConfig} from '../../Rest';
-import SdkExtension from '..';
+// import RingCentral from '../..';
+// import {AxiosResponse, Method} from 'axios';
+// import {RestRequestConfig} from '../../Rest';
+// import SdkExtension from '..';
 
-class WebSocketExtension extends SdkExtension {
-  install(rc: RingCentral): void {
-    rc.request = async <T>(
-      httpMethod: Method,
-      endpoint: string,
-      content?: {},
-      queryParams?: {},
-      config?: RestRequestConfig
-    ): Promise<AxiosResponse<T>> => {};
-  }
-}
+// class WebSocketExtension extends SdkExtension {
+//   install(rc: RingCentral): void {
+//     rc.request = async <T>(
+//       httpMethod: Method,
+//       endpoint: string,
+//       content?: {},
+//       queryParams?: {},
+//       config?: RestRequestConfig
+//     ): Promise<AxiosResponse<T>> => {};
+//   }
+// }
 
-export default WebSocketExtension;
+// export default WebSocketExtension;
