c50adc63b7a60dc0d58e6523ed194280e1b0c73f
Tyler Long <tyler4long@gmail.com>
2020-06-19 10:18:37 -0700

    Update wsg testing code

--- test/wsg.spec.ts
+++ test/wsg.spec.ts
@@ -3,55 +3,12 @@
 import waitFor from 'wait-for-async';
 
 import RingCentral from '../src/index';
-import {createRingCentral} from './utils';
 import WebSocketExtension from '../src/extensions/webSocket';
 
 jest.setTimeout(64000);
 
-const testSubscription = async (rc: RingCentral, wse: WebSocketExtension) => {
-  let eventCount = 0;
-  await wse.subscribe(
-    ['/restapi/v1.0/account/~/extension/~/message-store'],
-    event => {
-      expect(event).toBeDefined();
-      eventCount += 1;
-    }
-  );
-  await rc
-    .restapi()
-    .account()
-    .extension()
-    .sms()
-    .post({
-      from: {phoneNumber: process.env.RINGCENTRAL_USERNAME!},
-      to: [{phoneNumber: process.env.RINGCENTRAL_RECEIVER!}],
-      text: 'Hello world',
-    });
-  const successful = await waitFor({
-    condition: () => eventCount > 0,
-    interval: 1000,
-    times: 30,
-  });
-  await rc.revoke();
-  expect(successful).toBeTruthy();
-  expect(eventCount).toBeGreaterThan(0);
-};
-
 describe('WSG', () => {
-  test('Subscription with external token', async () => {
-    // assume that we got `externalToken` from somewhere else
-    const externalToken = (await createRingCentral()).token;
-    const rc = new RingCentral({
-      server: process.env.RINGCENTRAL_SERVER_URL!,
-      token: externalToken,
-    });
-    const websocketExtension = new WebSocketExtension({
-      server: process.env.RINGCENTRAL_WSG_SERVER_URL!,
-    });
-    rc.installExtension(websocketExtension);
-    await testSubscription(rc, websocketExtension);
-  });
-  test('subscription with self generated token', async () => {
+  test('subscription', async () => {
     const rc = new RingCentral({
       clientId: process.env.RINGCENTRAL_CLIENT_ID!,
       clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
@@ -66,8 +23,34 @@ describe('WSG', () => {
       server: process.env.RINGCENTRAL_WSG_SERVER_URL!,
     });
     rc.installExtension(websocketExtension);
-    await testSubscription(rc, websocketExtension);
+    let eventCount = 0;
+    await websocketExtension.subscribe(
+      ['/restapi/v1.0/account/~/extension/~/message-store'],
+      event => {
+        expect(event).toBeDefined();
+        eventCount += 1;
+      }
+    );
+    await rc
+      .restapi()
+      .account()
+      .extension()
+      .sms()
+      .post({
+        from: {phoneNumber: process.env.RINGCENTRAL_USERNAME!},
+        to: [{phoneNumber: process.env.RINGCENTRAL_RECEIVER!}],
+        text: 'Hello world',
+      });
+    const successful = await waitFor({
+      condition: () => eventCount > 0,
+      interval: 1000,
+      times: 30,
+    });
+    await rc.revoke();
+    expect(successful).toBeTruthy();
+    expect(eventCount).toBeGreaterThan(0);
   });
+
   test('Rest API call via WebSocket', async () => {
     const rc = new RingCentral({
       clientId: process.env.RINGCENTRAL_CLIENT_ID!,
