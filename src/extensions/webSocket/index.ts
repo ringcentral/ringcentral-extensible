25d4db218d77926f1ede9495f06f64a210e879e4
Tyler Long <tyler4long@gmail.com>
2020-06-25 19:14:41 -0700

    Refactor code

--- src/extensions/webSocket/index.ts
+++ src/extensions/webSocket/index.ts
@@ -10,40 +10,16 @@ import SdkExtension from '..';
 import RestException from '../../RestException';
 import {version} from '../../../package.json';
 import {SubscriptionInfo, CreateSubscriptionRequest} from '../../definitions';
-import {WsToken} from './types';
+import {
+  WsToken,
+  ConnectionDetail,
+  WebSocketOptions,
+  WsgEvent,
+  WsgMeta,
+  ConnectionBody,
+} from './types';
 
 const uuid = hyperid();
-
-export type WebSocketOptions = {
-  restOverWebSocket?: boolean;
-  debugMode?: boolean;
-};
-export type WsgEvent = {
-  data: string;
-};
-export type WsgMeta = {
-  type: 'ClientRequest' | 'ServerNotification' | 'Error' | 'ConnectionDetails';
-  messageId: string;
-  status: number;
-  headers: {
-    [key: string]: string;
-  };
-  wsc?: {
-    token: string;
-    sequence: number;
-  };
-};
-export type ConnectionBody = {
-  creationTime: string;
-  maxConnectionsPerSession: number;
-  recoveryBufferSize: number;
-  recoveryTimeout: number;
-  idleTimeout: number;
-  absoluteTimeout: number;
-};
-export type ConnectionDetail = WsgMeta & {
-  body: ConnectionBody;
-};
 
 class WebSocketExtension extends SdkExtension {
   static sandboxServer = 'wss://ws-api.devtest.ringcentral.com/ws';
