81ec9c53c67e6fc26eca8a9a24dbe109945c481d
Tyler Long <tyler4long@gmail.com>
2020-07-01 12:17:07 -0700

    reconnect but do not recover session

--- test/websocket/session-recovery.spec.ts
+++ test/websocket/session-recovery.spec.ts
@@ -71,6 +71,66 @@ describe('WSG session recovery', () => {
     expect(eventCount).toBeGreaterThan(0);
   });
 
+  test('reconnect by do not recover session ', async () => {
+    if (!process.env.IS_LAB_ENV) {
+      return;
+    }
+    const rc = new RingCentral({
+      clientId: process.env.RINGCENTRAL_CLIENT_ID!,
+      clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
+      server: process.env.RINGCENTRAL_SERVER_URL!,
+    });
+    await rc.login({
+      username: process.env.RINGCENTRAL_USERNAME!,
+      extension: process.env.RINGCENTRAL_EXTENSION!,
+      password: process.env.RINGCENTRAL_PASSWORD!,
+    });
+    const webSocketExtension = new WebSocketExtension({
+      // debugMode: true,
+      restOverWebSocket: true,
+    });
+    rc.installExtension(webSocketExtension);
+    await webSocketExtension.waitForReady();
+    expect(
+      webSocketExtension.connectionDetails?.body.recoveryState
+    ).toBeUndefined();
+    let eventCount = 0;
+    await webSocketExtension.subscribe(
+      ['/restapi/v1.0/account/~/extension/~/message-store/instant?type=SMS'],
+      event => {
+        expect(event).toBeDefined();
+        eventCount += 1;
+      }
+    );
+    // close WebSocket connection to simulate session lost
+    // here we don't invoke webSocketExtension.revoke() because that will also revoke all subscriptions created
+    webSocketExtension.ws.close();
+    await waitFor({interval: 5000});
+    await webSocketExtension.connect(false); // re-connect by do not recover session
+    await webSocketExtension.waitForReady();
+    expect(
+      webSocketExtension.connectionDetails?.body.recoveryState
+    ).toBeUndefined();
+    await rc
+      .restapi()
+      .account()
+      .extension()
+      .sms()
+      .post({
+        from: {phoneNumber: process.env.RINGCENTRAL_USERNAME!},
+        to: [{phoneNumber: process.env.RINGCENTRAL_USERNAME!}], // send sms to oneself
+        text: 'Hello world',
+      });
+    const successful = await waitFor({
+      condition: () => eventCount > 0,
+      interval: 1000,
+      times: 30,
+    });
+    await rc.revoke();
+    expect(successful).toBeTruthy();
+    expect(eventCount).toBeGreaterThan(0);
+  });
+
   test('re-connect existing session', async () => {
     if (!process.env.IS_LAB_ENV) {
       return;
