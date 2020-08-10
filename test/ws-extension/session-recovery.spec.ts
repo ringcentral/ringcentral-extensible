f1d95bbd94244d9db2bb6e3c4fb4e4aac9ae8f63
Tyler Long <tyler4long@gmail.com>
2020-08-10 09:49:11 -0700

    Update test case for session recovery

--- test/ws-extension/session-recovery.spec.ts
+++ test/ws-extension/session-recovery.spec.ts
@@ -4,6 +4,8 @@ import path from 'path';
 import dotenv from 'dotenv-override-true';
 import waitFor from 'wait-for-async';
 
+jest.setTimeout(999999999); // to test recover failed
+
 dotenv.config({path: path.join(__dirname, '..', '.env.lab')});
 
 describe('WSG session recovery', () => {
@@ -176,7 +178,8 @@ describe('WSG session recovery', () => {
     expect(successful).toBeTruthy();
     expect(eventCount).toBeGreaterThan(0);
   });
-  // todo: uncomment test case below
+
+  // // todo: uncomment test case below
   // test('too late to recover', async () => {
   //   if (process.env.IS_LAB_ENV !== 'true') {
   //     return;
@@ -199,44 +202,45 @@ describe('WSG session recovery', () => {
   //   expect(
   //     webSocketExtension.connectionDetails?.body.recoveryState
   //   ).toBeUndefined();
-  //   // let eventCount = 0;
-  //   // await webSocketExtension.subscribe(
-  //   //   ['/restapi/v1.0/account/~/extension/~/message-store/instant?type=SMS'],
-  //   //   event => {
-  //   //     expect(event).toBeDefined();
-  //   //     eventCount += 1;
-  //   //   }
-  //   // );
+  //   let eventCount = 0;
+  //   await webSocketExtension.subscribe(
+  //     ['/restapi/v1.0/account/~/extension/~/message-store/instant?type=SMS'],
+  //     event => {
+  //       expect(event).toBeDefined();
+  //       eventCount += 1;
+  //     }
+  //   );
   //   // close WebSocket connection to simulate session lost
   //   // here we don't invoke webSocketExtension.revoke() because that will also revoke all subscriptions created
   //   webSocketExtension.ws.close();
   //   await waitFor({
   //     interval:
   //       webSocketExtension.connectionDetails!.body.recoveryTimeout * 1000 +
-  //       20000,
+  //       600000,
   //   });
   //   await webSocketExtension.recover();
   //   console.log(JSON.stringify(webSocketExtension.connectionDetails, null, 2));
   //   expect(webSocketExtension.connectionDetails?.body.recoveryState).toBe(
   //     'Failed'
   //   );
-  //   // await rc
-  //   //   .restapi()
-  //   //   .account()
-  //   //   .extension()
-  //   //   .sms()
-  //   //   .post({
-  //   //     from: {phoneNumber: process.env.RINGCENTRAL_USERNAME!},
-  //   //     to: [{phoneNumber: process.env.RINGCENTRAL_USERNAME!}], // send sms to oneself
-  //   //     text: 'Hello world',
-  //   //   });
-  //   // const successful = await waitFor({
-  //   //   condition: () => eventCount > 0,
-  //   //   interval: 1000,
-  //   //   times: 30,
-  //   // });
+  //   eventCount = 0;
+  //   await rc
+  //     .restapi()
+  //     .account()
+  //     .extension()
+  //     .sms()
+  //     .post({
+  //       from: {phoneNumber: process.env.RINGCENTRAL_USERNAME!},
+  //       to: [{phoneNumber: process.env.RINGCENTRAL_USERNAME!}], // send sms to oneself
+  //       text: 'Hello world',
+  //     });
+  //   const successful = await waitFor({
+  //     condition: () => eventCount > 0,
+  //     interval: 1000,
+  //     times: 30,
+  //   });
   //   await rc.revoke();
-  //   // expect(successful).toBeTruthy();
-  //   // expect(eventCount).toBeGreaterThan(0);
+  //   expect(successful).toBeTruthy();
+  //   expect(eventCount).toBeGreaterThan(0);
   // });
 });
