7470d7569d02adb69dea439aa3a4fb2ca995af2e
Tyler Long <tyler4long@gmail.com>
2020-09-15 16:29:15 -0700

    First successful EV api call

--- packages/extensions/engage-voice/index.ts
+++ packages/extensions/engage-voice/index.ts
@@ -1,11 +1,78 @@
 import RingCentral from '@rc-ex/core';
 import SdkExtension from '@rc-ex/core/lib/SdkExtension';
+import RestException from '@rc-ex/core/lib/RestException';
+import axios, {AxiosInstance} from 'axios';
+import qs from 'qs';
+
+import {version} from './package.json';
+import {
+  RestMethod,
+  RestRequestConfig,
+  RestResponse,
+} from '@rc-ex/core/lib/Rest';
+
+type EngageVoiceOptions = {
+  server?: string;
+};
 
 class EngageVoiceExtension extends SdkExtension {
+  server: string;
   rc!: RingCentral;
+  httpClient!: AxiosInstance;
+
+  constructor(options?: EngageVoiceOptions) {
+    super();
+    this.server = options?.server ?? 'https://engage.ringcentral.com';
+  }
 
   async install(rc: RingCentral) {
     this.rc = rc;
+    this.httpClient = axios.create({
+      baseURL: this.server,
+      headers: {
+        'X-User-Agent': `${this.rc.rest.appName}/${this.rc.rest.appVersion} ringcentral-extensible/engage-voice/${version}`,
+      },
+      validateStatus: () => {
+        return true;
+      },
+    });
+  }
+
+  async request<T>(
+    method: RestMethod,
+    endpoint: string,
+    content?: {},
+    queryParams?: {},
+    config?: RestRequestConfig
+  ): Promise<RestResponse<T>> {
+    const _config: RestRequestConfig = {
+      method,
+      url: endpoint,
+      data: content,
+      params: queryParams,
+      ...config,
+    };
+    const r = await this.httpClient.request<T>(_config);
+    if (r.status >= 200 && r.status < 300) {
+      return r;
+    } else {
+      throw new RestException(r);
+    }
+  }
+
+  async authorize() {
+    if (this.rc.token?.access_token === undefined) {
+      throw new Error('Need a RingCentral access token to continue');
+    }
+    const r = await this.request(
+      'POST',
+      '/api/auth/login/rc/accesstoken',
+      qs.stringify({
+        rcTokenType: 'Bearer',
+        rcAccessToken: this.rc.token?.access_token,
+      })
+    );
+    console.log(r.data);
   }
 }
 
--- test/engage-voice-extension.spec.ts
+++ test/engage-voice-extension.spec.ts
@@ -1,1 +1,31 @@
-console.log('hello world');
+import RingCentral from '@rc-ex/core';
+import EngageVoiceExtension from '@rc-ex/engage-voice';
+import path from 'path';
+import dotenv from 'dotenv-override-true';
+
+dotenv.config({path: path.join(__dirname, '.env.ev')});
+
+describe('Engage Voice extension', () => {
+  test('default', async () => {
+    if (process.env.IS_EV_ENV !== 'true') {
+      return;
+    }
+    const rc = new RingCentral({
+      clientId: process.env.ENGAGE_VOICE_CLIENT_ID!,
+      clientSecret: process.env.ENGAGE_VOICE_CLIENT_SECRET!,
+      server: process.env.ENGAGE_VOICE_RC_SERVER_URL!,
+    });
+    await rc.authorize({
+      username: process.env.ENGAGE_VOICE_USERNAME!,
+      extension: process.env.ENGAGE_VOICE_EXTENSION!,
+      password: process.env.ENGAGE_VOICE_PASSWORD!,
+    });
+    const engageVoiceExtension = new EngageVoiceExtension({
+      server: process.env.ENGAGE_VOICE_SERVER_URL,
+    });
+    await rc.installExtension(engageVoiceExtension);
+    await engageVoiceExtension.authorize();
+
+    await rc.revoke();
+  });
+});
