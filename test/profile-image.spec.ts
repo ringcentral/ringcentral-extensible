046fbe872e747b8d34be5a6c553d491dff5e580b
Tyler Long <tyler4long@gmail.com>
2021-04-22 11:00:09 -0700

    Add a test case to download other's profile image

--- test/profile-image.spec.ts
+++ test/profile-image.spec.ts
@@ -23,6 +23,40 @@ describe('Profile image', () => {
     expect(buffer.constructor.name).toBe('Buffer');
     fs.writeFileSync(path.join(__dirname, 'temp.png'), buffer);
     await rc.revoke();
-    // We don't test WSG because it doesn't support binary downloading
+  });
+
+  test('download others', async () => {
+    const rc = new RingCentral({
+      clientId: process.env.RINGCENTRAL_CLIENT_ID!,
+      clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
+      server: process.env.RINGCENTRAL_SERVER_URL!,
+    });
+    await rc.login({
+      username: process.env.RINGCENTRAL_USERNAME!,
+      extension: process.env.RINGCENTRAL_EXTENSION!,
+      password: process.env.RINGCENTRAL_PASSWORD!,
+    });
+    const rc2 = new RingCentral({
+      clientId: process.env.RINGCENTRAL_CLIENT_ID!,
+      clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
+      server: process.env.RINGCENTRAL_SERVER_URL!,
+    });
+    await rc2.login({
+      username: process.env.RINGCENTRAL_USERNAME2!,
+      extension: process.env.RINGCENTRAL_EXTENSION2!,
+      password: process.env.RINGCENTRAL_PASSWORD2!,
+    });
+
+    // const buffer = await rc
+    //   .restapi()
+    //   .account()
+    //   .extension(rc2.token?.owner_id)
+    //   .profileImage()
+    //   .list();
+
+    // expect(buffer.constructor.name).toBe('Buffer');
+    // fs.writeFileSync(path.join(__dirname, 'temp.png'), buffer);
+
+    await rc.revoke();
   });
 });
