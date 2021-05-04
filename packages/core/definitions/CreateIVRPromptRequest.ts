f54c6e309fb69b6188eb831ceb9eab9ab5c40b30
Tyler Long <tyler4long@gmail.com>
2021-05-03 18:50:52 -0700

    Generate again

---
+++ packages/core/definitions/CreateIVRPromptRequest.ts
@@ -0,0 +1,17 @@
+import {Attachment} from './index';
+
+// Request body for operation createIVRPrompt
+class CreateIVRPromptRequest {
+  /**
+   * Audio file that will be used as a prompt. Attachment cannot be empty, only audio files are supported
+   * Required
+   */
+  attachment?: Attachment;
+
+  /**
+   * Description of file contents.
+   */
+  name?: string;
+}
+
+export default CreateIVRPromptRequest;
