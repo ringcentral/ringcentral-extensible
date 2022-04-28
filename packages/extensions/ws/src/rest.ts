19d2893b8bd4e22a410aa74be640072fff9110c4
Tyler Long <tyler4long@gmail.com>
2022-04-27 17:00:11 -0700

    Fix some style errors

--- packages/extensions/ws/src/rest.ts
+++ packages/extensions/ws/src/rest.ts
@@ -22,7 +22,7 @@ export async function request<T>(
   queryParams?: {},
   config?: RestRequestConfig,
 ): Promise<RestResponse<T>> {
-  const _config: RestRequestConfig = {
+  const newConfig: RestRequestConfig = {
     method,
     baseURL: this.wsToken?.uri,
     url: endpoint,
@@ -30,37 +30,35 @@ export async function request<T>(
     params: queryParams,
     ...config,
   };
-  _config.headers = {
-    ..._config.headers,
-    'X-User-Agent': `${this.rc.rest!.appName}/${
-      this.rc.rest!.appVersion
-    } ringcentral-extensible/ws/${version}`,
+  newConfig.headers = {
+    ...newConfig.headers,
+    'X-User-Agent': `${this.rc.rest!.appName}/${this.rc.rest!.appVersion} ringcentral-extensible/ws/${version}`,
   };
   const messageId = uuid();
   const requestBody = [
     {
       type: 'ClientRequest',
       messageId,
-      method: _config.method,
-      path: _config.url,
-      headers: _config.headers,
-      query: _config.params,
+      method: newConfig.method,
+      path: newConfig.url,
+      headers: newConfig.headers,
+      query: newConfig.params,
     },
   ];
-  if (_config.data) {
-    requestBody.push(_config.data);
+  if (newConfig.data) {
+    requestBody.push(newConfig.data);
   }
   await this.ws.send(JSON.stringify(requestBody));
   const [meta, body] = await Utils.waitForWebSocketMessage(
     this.ws,
-    (meta) => meta.messageId === messageId,
+    (_meta) => _meta.messageId === messageId,
   );
   const response: RestResponse = {
     data: body as T,
     status: meta.status,
     statusText: getReasonPhrase(meta.status),
     headers: meta.headers,
-    config: _config,
+    config: newConfig,
   };
   if (
     meta.type === 'ClientRequest'
