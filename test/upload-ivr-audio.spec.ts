ed259a1fe957b824aff6662d0beb54e2c6151ea2
Tyler Long <tyler4long@gmail.com>
2023-02-16 10:53:02 -0800

    IVR uploading is working now

--- test/upload-ivr-audio.spec.ts
+++ test/upload-ivr-audio.spec.ts
@@ -1,23 +1,22 @@
-// import fs from 'fs';
+import fs from 'fs';
 
 import { createRingCentral } from './utils';
 
 describe('IVR Audio', () => {
   test('upload', async () => {
     const rc = await createRingCentral();
-    // todo: https://jira.ringcentral.com/browse/PLA-73273
-    // await rc
-    //   .restapi()
-    //   .account()
-    //   .ivrPrompts()
-    //   .post({
-    //     name: 'Uploaded via API',
-    //     attachment: {
-    //       filename: 'test.mp3',
-    //       contentType: 'audio/mpeg',
-    //       content: fs.readFileSync('./test.mp3'),
-    //     },
-    //   });
+    await rc
+      .restapi()
+      .account()
+      .ivrPrompts()
+      .post({
+        name: 'Uploaded via API',
+        attachment: {
+          filename: 'test.mp3',
+          contentType: 'audio/mpeg',
+          content: fs.readFileSync('./test.mp3'),
+        },
+      });
     await rc.revoke();
   });
 });
