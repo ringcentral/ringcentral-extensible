73945d4b830d3cb27bc304e25588c1c77f23c749
Tyler Long <tyler4long@gmail.com>
2023-09-13 10:16:36 -0700

    Restore test case for forward all company calls

--- test/forward-all-company-calls.spec.ts
+++ test/forward-all-company-calls.spec.ts
@@ -1,13 +1,12 @@
-// import { createRingCentral } from './utils';
+import { createRingCentral } from './utils';
 
 describe('Forward all company calls', () => {
   test('patch', async () => {
-  // todo: uncomment, "FACC rule is not found."
-    //     const rc = await createRingCentral();
-    //     const r = await rc.restapi().account().forwardAllCalls().patch({
-    //       enabled: false,
-    //     });
-    //     expect(r.enabled).toBe(false);
-    //     await rc.revoke();
+    const rc = await createRingCentral();
+    const r = await rc.restapi().account().forwardAllCalls().patch({
+      enabled: false,
+    });
+    expect(r.enabled).toBe(false);
+    await rc.revoke();
   });
 });
