3800be3c14e5a0abd420323d41f2cc7274996c70
Tyler Liu <tyler4long@gmail.com>
2024-04-25 16:01:33 -0700

    Update auto refresh test case

--- test/auto-refresh-extension.spec.ts
+++ test/auto-refresh-extension.spec.ts
@@ -9,10 +9,14 @@ describe('Auto Refresh Extension', () => {
     const rc = await ReusableRestClient.getInstance();
     const autoRefreshExtension = new AutoRefreshExtension({interval: 1000 * 2}); // refresh every 2 seconds
     await rc.installExtension(autoRefreshExtension);
-    const accessToken = rc.token?.access_token;
+    let accessToken = rc.token?.access_token;
     autoRefreshExtension.start();
     await waitFor({interval: 1000 * 3}); // 3 seconds
     expect(rc.token?.access_token).not.toBe(accessToken);
+    await autoRefreshExtension.stop();
+    accessToken = rc.token?.access_token;
+    await waitFor({interval: 1000 * 3}); // 3 seconds
+    expect(rc.token?.access_token).toBe(accessToken);
     await autoRefreshExtension.revoke();
   });
 });
