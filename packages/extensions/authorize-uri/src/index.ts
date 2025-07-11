703b4bffc2b50a32719bbc0c76ee43aa4e8b05df
Tyler Liu <tyler4long@gmail.com>
2025-07-11 14:19:03 -0700

    Make authorize-uri work for both node and browser

--- packages/extensions/authorize-uri/src/index.ts
+++ packages/extensions/authorize-uri/src/index.ts
@@ -3,7 +3,7 @@ import SdkExtension from "@rc-ex/core/SdkExtension";
 // import type AuthorizeRequest from "@rc-ex/core/definitions/AuthorizeRequest";
 import type { QueryDataMap } from "urijs";
 import URI from "urijs";
-import { createHash, randomBytes } from "crypto";
+// import { createHash, randomBytes } from "crypto";
 
 export interface AuthorizeUriOptions {
   baseUri?: string;
@@ -19,6 +19,45 @@ type AuthorizeRequest = {
   state?: string;
   ui_options?: string;
 };
+
+function isNode() {
+  return typeof process !== "undefined" &&
+    process.versions?.node != null &&
+    typeof window === "undefined";
+}
+
+async function generateCodeVerifier(): Promise<string> {
+  const length = 32;
+  if (isNode()) {
+    const { randomBytes } = await import("crypto");
+    return randomBytes(length).toString("base64")
+      .replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
+  } else {
+    const array = new Uint8Array(length);
+    crypto.getRandomValues(array);
+    return btoa(String.fromCharCode(...array))
+      .replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
+  }
+}
+
+async function generateCodeChallenge(verifier: string): Promise<string> {
+  if (isNode()) {
+    const { createHash } = await import("crypto");
+    const hash = createHash("sha256").update(verifier).digest();
+    return hash.toString("base64").replace(/\+/g, "-").replace(/\//g, "_")
+      .replace(/=/g, "");
+  } else {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(verifier);
+    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashStr = String.fromCharCode(...hashArray);
+    return btoa(hashStr).replace(/\+/g, "-").replace(/\//g, "_").replace(
+      /=/g,
+      "",
+    );
+  }
+}
 
 class AuthorizeUriExtension extends SdkExtension {
   public rc!: RingCentral;
@@ -36,7 +75,7 @@ class AuthorizeUriExtension extends SdkExtension {
     this.rc = rc;
   }
 
-  public buildUri(_authorizeRequest: AuthorizeRequest): string {
+  public async buildUri(_authorizeRequest: AuthorizeRequest): Promise<string> {
     const authorizeRequest = _authorizeRequest;
     if (!authorizeRequest.response_type) {
       authorizeRequest.response_type = "code";
@@ -47,15 +86,19 @@ class AuthorizeUriExtension extends SdkExtension {
 
     // PKCE: https://medium.com/ringcentral-developers/use-authorization-code-pkce-for-ringcentral-api-in-client-app-e9108f04b5f0
     if (authorizeRequest.code_challenge_method === "S256") {
-      this.codeVerifier = randomBytes(32).toString("base64").replace(/\+/g, "-")
-        .replace(/\//g, "_").replace(/=/g, "");
-      authorizeRequest.code_challenge = createHash("sha256")
-        .update(this.codeVerifier)
-        .digest()
-        .toString("base64")
-        .replace(/\+/g, "-")
-        .replace(/\//g, "_")
-        .replace(/=/g, "");
+      this.codeVerifier = await generateCodeVerifier();
+      authorizeRequest.code_challenge = await generateCodeChallenge(
+        this.codeVerifier,
+      );
+      // this.codeVerifier = randomBytes(32).toString("base64").replace(/\+/g, "-")
+      //   .replace(/\//g, "_").replace(/=/g, "");
+      // authorizeRequest.code_challenge = createHash("sha256")
+      //   .update(this.codeVerifier)
+      //   .digest()
+      //   .toString("base64")
+      //   .replace(/\+/g, "-")
+      //   .replace(/\//g, "_")
+      //   .replace(/=/g, "");
     }
 
     let uri;
