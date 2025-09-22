6a3da7c4ab382bf8d889aa52e81821a453daa908
Tyler Liu <tyler4long@gmail.com>
2025-09-22 13:43:47 -0700

    Fix a FormData line breaks issue

--- packages/core/src/FormData.ts
+++ packages/core/src/FormData.ts
@@ -29,25 +29,26 @@ class FormData {
   public async getBody() {
     let buffer = Buffer.alloc(0);
     for (const formFile of this.files) {
+      if (buffer.length > 0) {
+        buffer = Buffer.concat([buffer, Buffer.from("\r\n", "utf-8")]);
+      }
       let temp = `--${boundary}\r\n`;
       temp += `Content-Type: ${formFile.contentType}\r\n`;
       temp +=
         `Content-Disposition: form-data; name="${formFile.name}"; filename="${formFile.filename}"\r\n\r\n`;
       buffer = Buffer.concat([buffer, Buffer.from(temp, "utf-8")]);
-      let fileBuffer = Buffer.alloc(0);
+      let fileBuffer: Buffer;
       if (typeof formFile.content === "string") {
-        fileBuffer = Buffer.from(`${formFile.content}\r\n`, "utf-8");
+        fileBuffer = Buffer.from(formFile.content, "utf-8");
       } else if (Buffer.isBuffer(formFile.content)) {
-        fileBuffer = formFile.content as Buffer<ArrayBuffer>;
+        fileBuffer = formFile.content;
       } else if (formFile.content instanceof Blob) {
-        fileBuffer = Buffer.from(
-          await formFile.content.arrayBuffer(),
-        ) as Buffer<ArrayBuffer>;
+        fileBuffer = Buffer.from(await formFile.content.arrayBuffer());
       } else {
         // NodeJS.ReadableStream
         fileBuffer = await stream2buffer(
           formFile.content as unknown as Stream,
-        ) as Buffer<ArrayBuffer>;
+        );
       }
       buffer = Buffer.concat([buffer, fileBuffer]);
     }
