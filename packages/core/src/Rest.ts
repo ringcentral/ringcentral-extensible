e96be593304a78970a009312aa58d3bb13c126bb
Tyler Liu <tyler4long@gmail.com>
2026-08-31 15:10:25 -0700

    Release 1.8.6

--- packages/core/src/Rest.ts
+++ packages/core/src/Rest.ts
@@ -11,7 +11,7 @@ import type {
   RestResponse,
 } from "./types.js";
 
-const version = "1.8.5";
+const version = "1.8.6";
 
 export default class Rest {
   public static sandboxServer = "https://platform.devtest.ringcentral.com";
